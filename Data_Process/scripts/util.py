18f8b3bb48d0136503926edf04362819b60a10f0
Mindy Zhou <40368009+mzhou356@users.noreply.github.com>
2020-08-04 11:21:01 -0400

    Update util.py
    modify dev

--- Data_Process/scripts/util.py
+++ Data_Process/scripts/util.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+
 def readData(dirpath,ext):
     """
     This function loads in all files with file type ext and merge into one pandas dataframe. 
@@ -27,3 +28,46 @@ def readData(dirpath,ext):
             datas.append(df)
     return pd.concat(datas,axis=0)
 
+def EDA_CAT_func(colname,target,df,fontsize, figsize,layout):
+    """
+    This function performs basic exploratory data analyses on a specific feature column,
+    This column needs to be categorical in nature. 
+    
+    Args:
+    colname: a string, the feature column name. 
+    target: a string, the target column name.
+    df: a pandas dataframe, the original data. 
+    fontsize: fontsize for boxplot
+    figsize: figsize for boxplot
+    layout: arrangement of row and col for boxplot
+    
+    Outputs the count for each category, boxplot for each category, and line charts for 
+    median trends. 
+    
+    return:
+    returns count by category dataframe. 
+    """
+    groupedDF = df.groupby(colname)
+    count_df = groupedDF[target].count().reset_index().sort_values(target)
+    groupedDF.boxplot(column=target,fontsize=fontsize,
+                                       figsize=figsize,layout=layout)
+    plt.show()
+    groupedDF[target].median().reset_index().plot.scatter(x=colname,y=target)
+    plt.show()
+    return count_df
+
+def binning_func(col,thresholds):
+    """
+    This function bins numerical features into categories. 
+    
+    Args:
+    col: a numeric value for the feature column
+    thresholds: a dictionary for map between threshold and categorical value 
+    df: a pandas dataframe, the original data.
+    
+    return:
+    transformed col as a categorical object type.
+    """
+    for k, v in thresholds.items():
+        if col>=k[0] and col<=k[1]:
+            return v
