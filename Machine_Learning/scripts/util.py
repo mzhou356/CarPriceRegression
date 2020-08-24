d3387cd15738185cf766c46f9d3946ff38f74f2b
Mindy <minzhou16@gmail.com>
2020-08-24 18:22:26 -0400

    Added func to set up the model for categorical embedding
    Signed-off-by: Mindy <minzhou16@gmail.com>

--- Machine_Learning/scripts/util.py
+++ Machine_Learning/scripts/util.py
@@ -274,3 +274,64 @@ def cate_embed_process(X_train,X_dev,X_test,embed_cols):
     input_list_dev.append(X_dev[other_cols].values)
     input_list_test.append(X_test[other_cols].values)
     return input_list_train,input_list_dev,input_list_test
+
+def embed_model_setup(embed_cols,X_train,dense_size,dense_output_size,dropout,metrics):
+    """
+    This function sets up models, one embed layer for each categorical feature and merge with other models 
+    
+    Args:
+    embed_cols: a list of string, feature name for embeded columns 
+    X_train: pandas df, features for training 
+    numeric_types: a list of types for each column 
+    dense_size: a list of input hidden node size 
+    dense_output_size: a list of output hidden node size 
+    droput ratio: for drop out layer, a list 
+    metrics: metrics for optimizing models 
+    
+    Returns:
+    Embeded neural network model 
+    """
+    input_models = [] 
+    output_embeddings = [] 
+    
+    for c in embed_cols:
+        c_emb_name = c+"_embedding"
+        num_unique = X_train[c].nunique()
+        if num_unique > 50:
+            embed_size = int(min(np.ceil(num_unique/2),50)) # half or 50 max 
+        else:
+            embed_size = num_unique
+        input_model = tfkl.Input(shape=(1,)) # one categorical features at a time for embed 
+        output_model = tfkl.Embedding(num_unique, embed_size, name = c_emb_name)(input_model) 
+        print(output_model.shape)
+        output_model = tfkl.Reshape(target_shape=(embed_size,))(output_model)
+        print(output_model.shape)
+        
+        # adding all categorical inputs 
+        input_models.append(input_model)
+        
+        # append all embeddings 
+        output_embeddings.append(output_model)
+    #  train other features with one NN layer 
+    input_numeric = tfkl.Input(shape=(len([c for c in X_train.columns if c not in embed_cols]),))
+    for i, size in enumerate(dense_size):
+        if i == 0:
+            embed_numeric = tfkl.Dense(size)(input_numeric)
+        else:
+            embed_numeric = tfkl.Dense(size)(embed_numeric)
+    input_models.append(input_numeric)
+    output_embeddings.append(embed_numeric)
+    
+    # add everythign together at the end 
+    output = tfkl.Concatenate()(output_embeddings)
+    for i, size in enumerate(dense_output_size):
+        output = tfkl.Dense(size,kernel_initializer="uniform",activation = tf.nn.leaky_relu)(output)
+        if i < len(dense_output_size)-1:
+            output = tfkl.Dropout(dropout[i])(output)
+    output = tfkl.Dense(1,activation="linear")(output)
+    
+    model = tfk.models.Model(inputs = input_models, outputs = output)
+    model.compile(loss="mse",optimizer="Adam",metrics=metrics)
+    return model 
+     
+    
