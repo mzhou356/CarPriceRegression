6e98ffb678ffe28fd021c5ebc544197bdf256951
Mindy <minzhou16@gmail.com>
2020-09-09 22:48:49 -0400

    added NN and embed class
    Signed-off-by: Mindy <minzhou16@gmail.com>

--- Machine_Learning/scripts/carPrice.py
+++ Machine_Learning/scripts/carPrice.py
@@ -1,11 +1,13 @@
 ### This creates a carPrice Class for machine learning 
 import pandas as pd
+import graphviz
 import numpy as np
 import tensorflow
 import tensorflow.compat.v2 as tf 
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from sklearn.metrics import r2_score,mean_squared_error
+from sklearn import tree
 from sklearn.model_selection import train_test_split, GridSearchCV
 tensorflow.compat.v1.logging.set_verbosity(tensorflow.compat.v1.logging.ERROR)
 
@@ -199,13 +201,32 @@ class treeCarPrice(carPrice):
         model = self.regressor 
         return model.feature_importances_
     
+    def tree_plot(self,trimmed=False):
+        """
+        This function outputs decision tree plot 
+    
+        args:
+        model: decision tree or random forest 
+        features_names: a list of feature names.
+        """
+        model = self.regressor
+        if trimmed:
+            feature_names = self.trimmed_features
+        else:
+            feature_names = self.features
+        dot_data = tree.export_graphviz(model,feature_names=feature_names,
+                                    rounded=True)
+        graph = graphviz.Source(dot_data,format="png")
+        return graph
+    
 class NNCarPrice(carPrice):
-    def __init__(self,data,NN_model,batch_size,epochs,callbacks):
-        super().__init(data,NN_model)
+    def __init__(self,data,NN_model=None,batch_size,epochs,callbacks,layers):
+        super().__init(data,NN_mode)
         self.history = None;
         self.batch_size = batch_size
         self.epochs = epochs
         self.callbacks = callbacks
+        self.layers = layers
         
     def data_split(self,seed,test_size,trimmed = False):
         X_train,X_test,y_train,y_test = super().data_split(seed,test_size,trimmed)
@@ -227,6 +248,229 @@ class NNCarPrice(carPrice):
         model = self.regressor 
         return model.predict(x,batch_size=self.batch_size).flatten()
     
-    def calculateCoef(self):
-        model = self.regressor 
-        return model.coef_
+    @staticmethod
+    def set_gpu_limit(n):
+        """
+        This function sets the max num of GPU in G to minimize overuse GPU per session.
+    
+        args:
+        n: a float, num of GPU in G.
+        """
+        gpus = tf.config.experimental.list_physical_devices("GPU")
+        tf.config.experimental.set_virtual_device_configuration(gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*n)]) 
+        
+    def make_tensor_dataset(self,X,y)
+        """
+        This function generates tensorflow train and test dataset for NN.
+    
+        args:
+        X: a pandas dataframes, features 
+        y: a pandas series, label 
+      
+    
+        returns:
+        tensforflow dataset
+        """
+        data_set = tf.data.Dataset.from_tensor_slices((X.values,
+                 y.values)).batch(self.batch_size).prefetch(tf.data.experimental.AUTOTUNE).shuffle(X.shape[0])
+        return data_set  
+    
+    def make_model(self,sizes,input_size, metrics, l2 = 10e-5, lr = 1e-4):
+        """
+        This function creates tensorflow regression model and use l2 to regularize the 
+        activate output. 
+    
+        Args:
+        sizes: a list of integer indicating num of hidden nodes
+        input_size: num of input features, an integer
+        l2:option input for activation regularizer to prevent overfitting of training data 
+        lr: for optimizer adam, gradient descent step size 
+        metrics: metrics for optimizing the model during tuning 
+        """
+        layers = [tfkl.InputLayer(input_shape=input_size)]
+        for s in sizes:
+            layers.append(tfkl.Dense(units=s,activation=tf.nn.leaky_relu,activity_regularizer=tfk.regularizers.l2(l2)))
+        layers.append(tfkl.Dense(units=1))
+        model = tfk.Sequential(layers, name = "NN_regressor")
+        model.compile(optimizer = tf.optimizers.Adam(learning_rate=lr), 
+                  loss = "mse",
+                  metrics = metrics)
+        self.base = model
+        
+    def extract_weights(self,layer_num):
+        """
+        This function returns model weights for the specific model layer num
+        
+        Args:
+        model: NN trained model. 
+        layer_num: layer_num, an integer.
+        
+        Returns:
+        layer weights as numpy array 
+        """
+        model = self.regressor
+        weights_info = model.layers[layer_num]
+        return weights_info.weights[0].numpy()
+    
+     def calculateCoef(self):
+        """
+        This function estimates layer coefficients by estimating coefficients of the weights avg
+    
+        Args:
+        model: NN trained model. 
+        layers: layers of model 
+    
+        Returns:
+        An array of coefficients. 
+        """
+        mean_weights = extract_weights(0)
+        layers = self.layers
+        for i in range(1,layers):
+            weights = extract_weights(i)
+            mean_weights = np.dot(mean_weights,weights)
+        return mean_weights
+    
+    def plot_metrics(self,metric):
+        """
+        This function plots the metric value for train and test 
+    
+        Arg:
+        history: a tensorflow.python.keras.callbacks.History object 
+        metric: a string, type of metric to plot 
+    
+        Returns:
+        A plot that shows 2 overlapping loss versus epoch images. red is for test and blue is for train 
+    
+        """
+        history = self.history.history
+        plt.plot(history[metric], color="blue", label="train")
+        plt.plot(history[f"val_{metric}"], color="red", label="test")
+        plt.xlabel("epoch")
+        plt.ylabel(metric)
+        plt.title("model training results")
+        plt.legend(loc="best")
+        plt.show()
+        
+    def paramSearch(self,params,mdl_setup,train_data_set,dev_dataset,V,xlog=True,ylog=True,metrics="loss"):
+        """
+        Plots paramsearch for one epoch only
+    
+        Args:
+        params: a list of parameters 
+        mdl_setup: output of model_setup func, partial function 
+        xlog,ylog: scale for the graph 
+        """
+        metrics = []
+        for p in params:
+            hist = mdl_setup(p).fit(train_data_set, epochs=1,shuffle=True,verbose = V,
+                              validation_data=dev_dataset)
+        metric = hist.history[metrics][0]
+        metrics.append(metric)
+        plt.plot(params,metrics)
+        if xlog:
+            plt.xscale("log")
+        if ylog:
+            plt.yscale("log")
+        plt.show()
+        
+def embedCarPrice(NNCarPrice):
+    def cate_embed_process(self,X_train,X_dev,X_test,embed_cols):
+        """
+        This function transforms features using X_train data to match the format to train neural network 
+    
+        Args:
+        X_train: pandas df, features for training 
+        X_dev: pandas df, features for dev/validation
+        X_test: pandas df, features for test (hold out set)
+        embed_cols: a list of feature name for embeded columns 
+    
+        Returns:
+        a list of features for train, dev, and test 
+        """
+        input_list_train = []
+        input_list_dev = []
+        input_list_test = []
+    
+        for c in embed_cols:
+            raw_values = X_train[c].unique() # get num of unique categories for each categorical features 
+            val_map={} # map each categorical to an integer number for embedding 
+            for i in range(len(raw_values)):
+                val_map[raw_values[i]] = i+1 
+        # map all categories to a value based upon train value only 
+            input_list_train.append(X_train[c].map(val_map).values)
+            input_list_dev.append(X_dev[c].map(val_map).fillna(0).values)  # allow null value as its own categorical (class 0 for null)
+            input_list_test.append(X_test[c].map(val_map).fillna(0).values)
+        # add rest of columns 
+        other_cols = [c for c in X_train.columns if c not in embed_cols]
+        if len(other_cols) > 0:
+            input_list_train.append(X_train[other_cols].values)
+            input_list_dev.append(X_dev[other_cols].values)
+            input_list_test.append(X_test[other_cols].values)
+        return input_list_train,input_list_dev,input_list_test
+    
+    def embed_model_setup(self,embed_cols,X_train,dense_size,dense_output_size,dropout,metrics,lr,embed_size_multiplier=1.0):
+        """
+        This function sets up models, one embed layer for each categorical feature and merge with other models 
+    
+        Args:
+        embed_cols: a list of string, feature name for embeded columns 
+        X_train: pandas df, features for training 
+        numeric_types: a list of types for each column 
+        dense_size: a list of input hidden node size for numeric feature model 
+        dense_output_size: a list of output hidden node size after all embed layers added together 
+        droput ratio: for drop out layer, a list 
+        metrics: metrics for optimizing models 
+        lr: learning rate for adam optimizer 
+        embed_size_multiplier: float, adjusts embedsize 
+    
+        Returns:
+        Embeded neural network model 
+        """
+        input_models = [] 
+        output_embeddings = [] 
+    
+        for c in embed_cols:
+            c_emb_name = c+"_embedding"
+            num_unique = X_train[c].nunique()+1 # allow null value for label 0 
+        # use a formula from fastai
+            embed_size = int(min(50,num_unique/2*embed_size_multiplier))
+            input_model = tfkl.Input(shape=(1,)) # one categorical features at a time for embed 
+        # each input category gets an embed feature vector 
+            output_model = tfkl.Embedding(num_unique, embed_size, name = c_emb_name)(input_model) 
+#         print(output_model.shape)
+        # reshape embed model so each corresponding row gets its own feature vector 
+            output_model = tfkl.Reshape(target_shape=(embed_size,))(output_model)
+#         print(output_model.shape)
+        
+        # adding all categorical inputs 
+            input_models.append(input_model)
+        
+        # append all embeddings 
+            output_embeddings.append(output_model)
+        
+    #  train other features with one NN layer 
+        numeric_cols = [c for c in X_train.columns if c not in embed_cols]
+        if len(numeric_cols)>0:
+            input_numeric = tfkl.Input(shape=(len(numeric_cols),))
+            for i, size in enumerate(dense_size):
+                if i == 0:
+                    embed_numeric = tfkl.Dense(size)(input_numeric)
+                else:
+                    embed_numeric = tfkl.Dense(size)(embed_numeric)
+            input_models.append(input_numeric)
+            output_embeddings.append(embed_numeric)
+    
+    # add everything together at the end 
+    # add all output embedding nodes together as one layer 
+        output = tfkl.Concatenate()(output_embeddings)
+        for i, size in enumerate(dense_output_size):
+            output = tfkl.Dense(size,kernel_initializer="uniform",activation = tf.nn.leaky_relu)(output)
+        # not add drop out for last output layer 
+            if i < len(dense_output_size)-1:
+                output = tfkl.Dropout(dropout[i])(output)
+        output = tfkl.Dense(1,activation="linear")(output)
+    
+        model = tfk.models.Model(inputs = input_models, outputs = output)
+        model.compile(loss="mse",optimizer=tf.optimizers.Adam(learning_rate=lr),metrics=metrics)
+        self.base = model
