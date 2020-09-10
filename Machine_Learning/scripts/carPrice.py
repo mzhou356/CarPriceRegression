dc1d44b25a6593cf395b2d3bacc5b0cfb299cc0c
Mindy <minzhou16@gmail.com>
2020-09-09 21:33:12 -0400

    Modified carPrice to allow inheritance
    Signed-off-by: Mindy <minzhou16@gmail.com>

--- Machine_Learning/scripts/carPrice.py
+++ Machine_Learning/scripts/carPrice.py
@@ -14,22 +14,16 @@ tensorflow.compat.v1.logging.set_verbosity(tensorflow.compat.v1.logging.ERROR)
 tfk = tf.keras;
 tfkl=tf.keras.layers
 
-class carPrice():
-    def __init__(self,data, regressor, NN = False, tree = False,batch_size = None,embed=False):
+class carPrice:
+    def __init__(self,data, regressor):
         self.features = data.drop("price",axis=1)
         self.trimmed_features = None
         self.label = data.price 
         self.base = regressor 
         self.regressor = regressor 
-        self.NN = NN
-        self.embed = embed
-        self.Tree = tree
         self.tuned=False
         self.gridResult = None 
-        self.batch_size = batch_size
-        self.history = None 
-        
-        
+         
     def removeFeatures(self, n ):
         feature_table = self.linear_feature_importance(plot=False)
         features_remove = feature_table.head(n).features 
@@ -37,33 +31,39 @@ class carPrice():
         
     def data_split(self,seed,test_size,trimmed = False):
         if trimmed == True:
-            features = self.trimmed_features 
+            X_train, X_test, y_train, y_test = train_test_split(self.trimmed_features,self.label, 
+                                                                test_size = test_size, random_state=seed) 
         else:
-            features = self.features
-        X_train, X_test, y_train, y_test = train_test_split(features,self.label, test_size = test_size, random_state=seed)
+            X_train, X_test, y_train, y_test = train_test_split(self.features,self.label, 
+                                                                test_size = test_size, random_state=seed)
         return X_train,X_test,y_train,y_test 
     
-    def train_model(self,X=None,y=None,train_dataset = None,dev_dataset=None,epochs=None, V = 0, callbacks=None):
-        if self.tuned ==True:
+    def train_model(self,X,y):
+        if self.tuned:
             model = self.gridResult.best_estimator_
         else:
             model = self.base 
-        if self.NN:
-            if self.embed:
-                train_input,y_train = train_dataset
-                self.history = model.fit(train_input,y_train,epochs=epochs,
-                                         shuffle=True,verbose=V,validation_data=dev_dataset,
-                                         callbacks=callbacks)
-            else:
-                self.history = model.fit(train_dataset, epochs = epochs,
-                                     shuffle=True,verbose=V,validation_data=dev_dataset,
-                                     callbacks=callbacks)
-        else:
-            model.fit(X,y)
+        model.fit(X,y)
+#         if self.NN:
+#             if self.embed:
+#                 train_input,y_train = train_dataset
+#                 self.history = model.fit(train_input,y_train,epochs=epochs,
+#                                          shuffle=True,verbose=V,validation_data=dev_dataset,
+#                                          callbacks=callbacks)
+#             else:
+#                 self.history = model.fit(train_dataset, epochs = epochs,
+#                                      shuffle=True,verbose=V,validation_data=dev_dataset,
+#                                      callbacks=callbacks)
+#         else:   
         self.regressor = model 
+    
+    def calculate_pred(self,x,y,retrain=True):
+        if retrain:
+            self.train_model(x,y)
+        model = self.regressor 
+        return model.predict(x)
   
-    def regression_metrics(self,x_train,y_train,x_test,y_test,retrain=True,train_dataset=None,dev_dataset=None,epochs=None,
-                          V=0,callbacks=None):
+    def regression_metrics(self,x_train,y_train,x_test,y_test,retrain=True):
         """
         This function outputs model scores (r2 and root mean squared error)
     
@@ -77,19 +77,9 @@ class carPrice():
         r2 score for both train and test 
         root mean squared error for both train and test 
         as a pandas dataframe
-        """
-        if retrain:
-            if self.NN:
-                self.train_model(train_dataset=train_dataset,dev_dataset=dev_dataset,epochs=epochs,V=V, callbacks=callbacks)
-            else:
-                self.train_model(x_train,y_train)
-        model = self.regressor 
-        if self.batch_size != None:
-            pred_train = model.predict(x_train,batch_size=self.batch_size).flatten()
-            pred_test = model.predict(x_test,batch_size=self.batch_size).flatten()
-        else:
-            pred_train = model.predict(x_train)
-            pred_test = model.predict(x_test)
+        """     
+        pred_train = self.calculate_pred(x_train,y_train,retrain)
+        pred_test = self.calculate_pred(x_test,y_test,retrain)
         r2_train = r2_score(y_train,pred_train)
         r2_test = r2_score(y_test,pred_test)
         rmse_train = np.sqrt(mean_squared_error(y_train,pred_train))
@@ -102,13 +92,17 @@ class carPrice():
                                           
         return metric_table
     
-    def paramSearch(self,params,X,y):
-        searchGrid = GridSearchCV(self.base,params,scoring = "neg_root_mean_squared_error",n_jobs=22, verbose=1)
+    def paramSearch(self,params,X,y,V=1):
+        searchGrid = GridSearchCV(self.base,params,scoring = "neg_root_mean_squared_error",n_jobs=22, verbose=V)
         searchGrid.fit(X,y)
         self.tuned = True
         self.gridResult = searchGrid
         
-    def linear_feature_importance(self,plot=True,NN_coefs = None):
+    def calculateCoef(self):
+        model = self.regressor 
+        return model.coef_
+        
+    def linear_feature_importance(self,plot=True):
         """
         This function creates model feature importance for linear regression
     
@@ -121,13 +115,14 @@ class carPrice():
         returns:
         feature importance pandas dataframe and a bar plot
         """
-        model = self.regressor 
-        if self.NN:
-            coefs = NN_coefs
-        elif self.Tree:
-            coefs = model.feature_importances_
-        else:
-            coefs = model.coef_
+#         model = self.regressor 
+# #         if self.NN:
+# #             coefs = NN_coefs
+# #         elif self.Tree:
+# #             coefs = model.feature_importances_
+# #         else:
+#         coefs = model.coef_
+        coefs = self.calculateCoef();
         table = pd.DataFrame({"features":self.features.columns,"score":np.abs(coefs)})
         if plot:
             table.sort_values("score",ascending=False).head(20).plot.barh(x="features",y="score",figsize=(6,8),label="coef")
@@ -140,7 +135,7 @@ class carPrice():
             plt.show()
         return table.sort_values("score") 
     
-    def price_diff(self,embed_input_list=None,embed_y=None,trimmed=False):
+    def price_diff(self,trimmed=False):
         """
         This function outputs a dataframe with price diff info 
     
@@ -160,20 +155,19 @@ class carPrice():
             result_table = self.trimmed_features.copy()
         else:
             result_table = self.features.copy()
-        if self.NN:
-            if self.embed:
-                label = embed_y
-                pred_price = model.predict(embed_input_list, batch_size=self.batch_size).flatten()
-            else:
-                pred_price = model.predict(result_table.values, batch_size=self.batch_size).flatten()
-        else:
-            pred_price = model.predict(result_table)
+#         if self.NN:
+#             if self.embed:
+#                 label = embed_y
+#                 pred_price = model.predict(embed_input_list, batch_size=self.batch_size).flatten()
+#             else:
+#                 pred_price = model.predict(result_table.values, batch_size=self.batch_size).flatten()
+        pred_price = self.calculate_pred(result_table,label,retrain=False)
         diff = (pred_price-label)/label*100
         result_table["price_diff_pct"]=diff
         result_table["price_diff_abs"]=np.abs(diff)
         return result_table.sort_values("price_diff_abs",ascending=False)
     
-    def plot_pred_price(self,X_embed=None,y_embed=None,trimmed=False):
+    def plot_pred_price(self,trimmed=False):
         """
         This funciton plots predicted price vs actual price, with a r2 score 
         Also plots residual value distribution 
@@ -190,14 +184,13 @@ class carPrice():
             features = self.features
         model = self.regressor
         y = self.label
-        if self.NN:
-            if self.embed:
-                y = y_embed
-                pred = model.predict(X_embed, batch_size=self.batch_size).flatten()
-            else:
-                pred = model.predict(features.values,batch_size=self.batch_size).flatten()
-        else:
-            pred = model.predict(features)
+#         if self.NN:
+#             if self.embed:
+#                 y = y_embed
+#                 pred = model.predict(X_embed, batch_size=self.batch_size).flatten()
+#             else:
+#                 pred = model.predict(features.values,batch_size=self.batch_size).flatten()
+        pred = self.calculate_pred(features,y,retrain=False)
         r2 = r2_score(y,pred)
         sns.jointplot(y,pred,label=f"r2_score:{r2}",kind="reg")
         plt.xlabel("price")
@@ -207,3 +200,38 @@ class carPrice():
         sns.distplot((pred-y))
         plt.xlabel("error(pred-price)")
         plt.show()
+        
+class treeCarPrice(carPrice):
+    def calculateCoef(self):
+        model = self.regressor 
+        return model.feature_importances_
+    
+class NNCarPrice(carPrice):
+    def __init__(self,data,NN_model,batch_size,epochs,callbacks):
+        super().__init(data,NN_model)
+        self.history = None;
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.callbacks = callbacks
+        
+    def data_split(self,seed,test_size,trimmed = False):
+        X_train,X_test,y_train,y_test = super().data_split(seed,test_size,trimmed)
+        X_train,X_dev,y_train,y_dev = train_test_split(X_train,y_train,
+                                                       test_size = test_size,random_state=seed)
+        return X_train,X_test,y_train,y_test,X_dev,y_dev
+    
+        
+    def train_model(self,train_dataset,dev_dataset,V):
+        model = self.base 
+        self.history = model.fit(train_dataset, epochs = self.epochs,
+                                     shuffle=True,verbose=V,validation_data=dev_dataset,
+                                     callbacks=self.callbacks)
+        self.regressor = model
+        
+    def calculate_pred(self,x,train_dataset,dev_dataset,V,retrain=True):
+        if retrain:
+            self.train_model(train_dataset,dev_dataset=dev_dataset,V=V)
+        model = self.regressor 
+        return model.predict(x,batch_size=self.batch_size).flatten()
+                                   
+    
