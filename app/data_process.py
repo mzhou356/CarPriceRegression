a1c9ecd316b3d554a45a0c546026b7782e2b8280
Mindy <minzhou16@gmail.com>
2020-10-06 23:02:44 -0400

    created an app folder for prediction
    Signed-off-by: Mindy <minzhou16@gmail.com>

---
+++ app/data_process.py
@@ -0,0 +1,12 @@
+"""
+1. This script reads input from user as a json object.
+2. Processes the data into feature format for prediction.
+"""
+import json 
+
+# assume this is user input 
+feature_dict = json.load("user_input.json")
+
+
+
+
